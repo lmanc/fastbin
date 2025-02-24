fc87232fe820912080d15ba102a7827b71d6ff94
Luca Mancusi <luca.mancusi@protonmail.ch>
2025-02-24 22:42:01 +0100

    Add logging for GET request in methods.py

--- app/api/routes/methods.py
+++ app/api/routes/methods.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter, Request
+from app.api.core.logger import logger
 
 router = APIRouter()
 
 
 @router.get("/get")
 async def get(request: Request):
+    logger.info(f"Received GET request from {request.client.host}")
+
     return {
         "args": dict(request.query_params),
         "headers": dict(request.headers),
