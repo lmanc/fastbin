65803c79e1357c417834c439acd3031e69eba6cd
Luca Mancusi <luca.mancusi@protonmail.ch>
2025-02-27 21:54:41 +0100

    Add validation on response

--- app/api/routers/methods.py
+++ app/api/routers/methods.py
@@ -1,15 +1,13 @@
 from fastapi import APIRouter, Request
-from app.api.core.logger import logger
+from app.api.core.models import CoreResponse
 
 router = APIRouter()
 
 
 @router.get("/get")
-async def get(request: Request):
-    logger.info(f"Received GET request from {request.client.host}")
-
+async def get(request: Request) -> CoreResponse:
     return {
-        "args": dict(request.query_params),
+        "query_params": dict(request.query_params),
         "headers": dict(request.headers),
         "origin": request.client.host,
         "url": str(request.url),
