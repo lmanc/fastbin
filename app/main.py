5625cd8efb26df87a49f819c25872ab8c2a4fdfb
pygaiwan <moise.medici@gmail.com>
2025-02-27 21:39:43 +0000

    Double quotes replacement format

--- app/main.py
+++ app/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
+
 from app.api.routers import methods
 
 app = FastAPI(
-    title="FastBin",
-    version="0.1.0",
-    swagger_ui_parameters={"tryItOutEnabled": True},
+    title='FastBin',
+    version='0.1.0',
+    swagger_ui_parameters={'tryItOutEnabled': True},
 )
 
 
