6bc1f24c8437e8ce09cfbe80dfdd9f7b06e282c5
Luca Mancusi <luca.mancusi@protonmail.ch>
2025-03-01 17:37:14 +0100

    Reformat

--- app/api/routers/methods.py
+++ app/api/routers/methods.py
@@ -8,6 +8,7 @@ router = APIRouter()
 @router.get('/get')
 async def get(request: Request) -> CoreResponse:
     logger.info(f'Received {request.method} request on {request.url} from {request.client.host}')
+
     return {
         'query_params': dict(request.query_params),
         'headers': dict(request.headers),
