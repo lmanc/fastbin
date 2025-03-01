cf7a3459567b9399fa38a85380fabb5d99a9bd6c
Luca Mancusi <luca.mancusi@protonmail.ch>
2025-03-01 17:37:01 +0100

    Refactor CoreResponse model by removing Headers class and updating type annotations

--- app/api/core/models.py
+++ app/api/core/models.py
@@ -1,19 +1,10 @@
-from pydantic import BaseModel, Field, HttpUrl, IPvAnyAddress
-
-
-class Headers(BaseModel):
-    host: str
-    connection: str
-    user_agent: str = Field(alias='user-agent')
-    accept: str
-    accept_language: str = Field(alias='accept-language', default=None)
-    referer: str | None = None
-    accept_encoding: str = Field(alias='accept-encoding')
-    cookie: str | None = None
+from pydantic import BaseModel, HttpUrl, IPvAnyAddress
 
 
 class CoreResponse(BaseModel):
-    query_params: dict
-    headers: Headers
+    """Core response to return the same data for each request."""
+
+    query_params: dict[str, str]
+    headers: dict[str, str]
     origin: IPvAnyAddress
     url: HttpUrl
