e5de569ab2d6efbb27d620075ecb92ee8c0c325f
Luca Mancusi <luca.mancusi@protonmail.ch>
2025-03-02 00:39:26 +0100

    Improve test for /get endpoint with status check and response validation

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -1,3 +1,11 @@
+from fastapi import status
+
+from app.api.core import CoreResponse
+
+
 def test_get(client):
+    """Test /get endpoint."""
     response = client.get('/get')
-    assert response.status_code == 200
+
+    assert response.status_code == status.HTTP_200_OK
+    assert CoreResponse.model_validate(response.json())
