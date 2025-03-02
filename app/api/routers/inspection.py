924ca1da4002dbd25543eb66e6f652e88d079be8
Luca Mancusi <luca.mancusi@protonmail.ch>
2025-03-02 10:52:46 +0100

    Fix summary punctuation for headers endpoint

--- app/api/routers/inspection.py
+++ app/api/routers/inspection.py
@@ -5,7 +5,7 @@ from app.api.core import HeadersResponse, logger
 router = APIRouter()
 
 
-@router.get('/headers', summary="Return the incoming request's HTTP headers")
+@router.get('/headers', summary="Return the incoming request's HTTP headers.")
 async def headers(request: Request) -> HeadersResponse:
     """Headers endpoint."""
     logger.info(f'Received {request.method} request on {request.url} from {request.client.host}')
